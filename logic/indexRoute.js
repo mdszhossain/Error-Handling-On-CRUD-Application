4f25f997fed994d5bfa682b02c0599f0421f0a63
mdszhossain <personalmailshazzad@gmail.com>
2026-07-14 15:16:21 +0600

    Update: New Custom Error Throwing from Indexroute.js file. Custom Error Class Created.

--- logic/indexRoute.js
+++ logic/indexRoute.js
@@ -1,8 +1,9 @@
 const Employee = require("../models/employee");
+const ExpressError = require("../ExpressError");
 const indexLogic = async(req, res, next) => {
     const employees = await Employee.find({});
-    if(!employees) {
-        return next(err);
+    if(employees.length === 0) {
+        throw new ExpressError(404, "No Employee Found");
     }
     res.send(employees);
 }
