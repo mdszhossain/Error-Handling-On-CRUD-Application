f0dd6caaf8c6d992be89063d1a62c82a46e8255d
mdszhossain <personalmailshazzad@gmail.com>
2026-07-14 12:00:33 +0600

    Sampale data file updated and expoorted

---
+++ database/data.js
@@ -0,0 +1,64 @@
+const employees = [
+    {
+        empName: "Md Shazzad Hossain",
+        empDesignation: "Software Engineer",
+        empSalary: 100000,
+        empContact: "01766032333"
+    },
+    {
+        empName: "Nusrat Jahan",
+        empDesignation: "Senior Software Engineer",
+        empSalary: 140000,
+        empContact: "01812345678"
+    },
+    {
+        empName: "Tanvir Rahman",
+        empDesignation: "Product Manager",
+        empSalary: 120000,
+        empContact: "01987654321"
+    },
+    {
+        empName: "Sanjida Akter",
+        empDesignation: "UI/UX Designer",
+        empSalary: 85000,
+        empContact: "01677889900"
+    },
+    {
+        empName: "Fahim Faisal",
+        empDesignation: "QA Engineer",
+        empSalary: 75000,
+        empContact: "01511223344"
+    },
+    {
+        empName: "Arifur Rahman",
+        empDesignation: "DevOps Engineer",
+        empSalary: 115000,
+        empContact: "01311224455"
+    },
+    {
+        empName: "Sadia Islam",
+        empDesignation: "Data Analyst",
+        empSalary: 90000,
+        empContact: "01711223344"
+    },
+    {
+        empName: "Mahmudul Hasan",
+        empDesignation: "Frontend Developer",
+        empSalary: 95000,
+        empContact: "01822334455"
+    },
+    {
+        empName: "Ayesha Siddiqua",
+        empDesignation: "HR Manager",
+        empSalary: 80000,
+        empContact: "01933445566"
+    },
+    {
+        empName: "Imran Khan",
+        empDesignation: "Solutions Architect",
+        empSalary: 180000,
+        empContact: "01544556677"
+    }
+];
+
+module.exports = {data: employees};
